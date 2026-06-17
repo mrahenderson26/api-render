78430a25f8ccc6d9ccc7487a326bf3347afae9e3
mrahenderson26 <mrahenderson26@gmail.com>
2026-06-17 11:32:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+ res.json("Bienvenidos a nuestra API");
+});
+
+
+
+app.listen(PORT, () => {
+ console.log(`Servidor iniciado en puerto ${PORT}`);
+});
