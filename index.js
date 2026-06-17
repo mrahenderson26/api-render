71ae263b71a8c474906d4d745743772d6e87f4fb
mrahenderson26 <mrahenderson26@gmail.com>
2026-06-17 12:40:43 +0200

    class

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: "https://grand-custard-ed2051.netlify.app/"
 }));
 
 const PORT = process.env.PORT || 3000;
