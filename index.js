6909b3090eec4084af75af94b47bd58b056314d9
mrahenderson26 <mrahenderson26@gmail.com>
2026-06-17 11:59:07 +0200

    update

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ app.use(express.json());
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
- res.json("Bienvenidos a nuestra API");
+ res.json("Bienvenidos a nuestra API - Hemos hecho una modificación exitosa");
 });
-
-
 
 app.listen(PORT, () => {
  console.log(`Servidor iniciado en puerto ${PORT}`);
