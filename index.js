739fb1409e0b664efd90082ba832d0b83d9e2729
mrahenderson26 <mrahenderson26@gmail.com>
2026-06-17 12:34:44 +0200

    class

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://api-render-vvdo.onrender.com/"
+    origin: "http://localhost:5173"
 }));
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.json("Probando con React");
+  res.json({
+    mensaje: "Probando con React"
+  });
 });
 
 app.listen(PORT, () => {
