5327a588e0745779f1f702230441e6c841f24822
mrahenderson26 <mrahenderson26@gmail.com>
2026-06-17 12:31:11 +0200

    class

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: "https://mi-app.netlify.app"
+    origin: "https://api-render-vvdo.onrender.com/"
 }));
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.json("Probando con React otra vez");
+    res.json("Probando con React");
 });
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en puerto ${PORT}`);
 });
-
-
 
