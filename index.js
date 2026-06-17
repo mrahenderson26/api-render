fc65d7f753e5850819ddbe1d67f82d6ff6b9c3dd
mrahenderson26 <mrahenderson26@gmail.com>
2026-06-17 12:28:50 +0200

    class

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ import cors from "cors"
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "https://mi-app.netlify.app"
+}));
 
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.json("Probando con React");
+    res.json("Probando con React otra vez");
 });
 
 app.listen(PORT, () => {
